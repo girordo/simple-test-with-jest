b42db431be9116eaf80271bc44431513d6d31b78
Tarcisio Giroldo <tarcisio.siqueira@usp.br>
2021-06-02 23:56:11 -0300

    :sparkles: | hehe headerrrrrr

---
+++ src/components/Header/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Image from "next/image";
+import Link from "next/link";
+
+const Header = () => {
+  return (
+    <header>
+      <Image />
+      <Link></Link>
+    </header>
+  );
+};
+
+export default Header;
